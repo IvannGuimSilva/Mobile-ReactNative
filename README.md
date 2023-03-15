5f794de2efac3b350bc75cb7ad8bf1a9c64b71ec
IvannGuimSilva <ivanguimsilva2504@gmail.com>
2023-03-14 23:50:45 -0300

    Repo has been built again :3

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tarefas de React Native pedidas pelo professor Joseffe
+
+Esse repositório é o local onde estão localizados todos os projetos feitos em React Native pedidos pelo professor durante as aulas.
---
+++ app13_perfilTab/src/Components/Pessoal/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { 
+  Text, 
+  View, 
+  Image, 
+  ScrollView, 
+  Pressable, 
+  Linking 
+} from 'react-native'
+import style from './style'
+
+export default Pessoal = () => {
+  const profile = {
+    name: "Ivan Guimarães",
+    login: "IvannGuimSilva",
+    bio: "Our self development's an infinite roadway, and that is cool!",
+    link: "https://github.com/IvannGuimSilva"
+  }
+  return(
+    <ScrollView style={style.phoneScreen}>
+      <View style={style.profileCard}>
+        <Image 
+          style={style.profileImg} 
+          source={require('./images/foto-perfil.jpeg')}
+        />
+        <View style={style.textsBox}>
+          <Text style={style.nome}>{profile.name}</Text>
+          <Text style={style.login}>{profile.login}</Text>
+          <Text style={style.bio}>{profile.bio}</Text>
+          <Pressable
+            onPress={ () => { Linking.openURL(profile.link) }}
+          >
+            <Text style={style.txtBtn}>
+              Veja meu perfil e projetos no Github
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </ScrollView>
+  )
+}
